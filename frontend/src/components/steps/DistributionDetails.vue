9ab48e766f57f1f143188ac4b38885338670e8fa
Omar Shibli <omarshib@gmail.com>
2017-08-30 18:01:00 +0300

    added DistributionDetails, PaymentDetails

--- frontend/src/components/steps/DistributionDetails.vue
+++ frontend/src/components/steps/DistributionDetails.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="content">
-
+    <div class="row">
+      <div class="input-group form-group">
+        <label class="pull-left title">Enter Ethereum return address</label>
+        <div class="col-xs-12">
+          <input class="form-control" type="text" v-model="etherumAddress" placeholder="Etherum address" />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-group form-group">
+        <label class="pull-left title">Which wallet are you using?</label>
+        <div class="col-xs-12">
+          <select class="form-control">
+            <option value="" selected disabled>Please select wallet provider</option>
+            <option value="my_ether_wallet">My Ether Wallet</option>
+          </select>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +27,11 @@
 export default {
   name: 'DistributionDetails',
   props: [],
+  data () {
+    return {
+      etherumAddress: null,
+    }
+  },
   components: {
   },
   computed: {
@@ -25,6 +48,10 @@ export default {
   font-size: 16px;
   line-height: 22px;
   text-align: center;
-  padding: 20px 10px;
+  padding: 50px;
+
+  .title {
+    padding-left: 20px;
+  }
 }
 </style>
--- frontend/src/components/steps/PaymentDetails.vue
+++ frontend/src/components/steps/PaymentDetails.vue
@@ -1,6 +1,45 @@
 <template>
   <div class="content">
-
+    <div class="row">
+      <div class="text-center cbt-title">
+        <h2 v-if="cbtAmount">{{ cbtAmount }} CBT</h2>
+        <h2 v-else>0.0 CBT</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="text-center">
+        <strong>1 CommerceBlock Token = 10 USD</strong>
+      </div>
+    </div>
+    <div class="row">
+      <div class="text-center">
+        <small>25% Preorder Bouns</small>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-group form-group">
+        <label class="pull-left title-usd-amount">Enter amount in USD</label>
+        <div class="col-xs-12">
+          <input class="form-control input-usd-amount" type="text" v-model="usdAmount" placeholder="Amount">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-group form-group">
+        <label class="pull-left title-usd-amount">How you would like to pay?</label>
+        <div class="col-xs-12">
+          <input class="form-control input-usd-amount" type="text" v-model="usdAmount" placeholder="Amount">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-3">
+        <label class="radio-inline"><input type="radio" name="currencyType">BTC</label>
+      </div>
+      <div class="col-xs-3">
+        <label class="radio-inline"><input type="radio" name="currencyType">ETH</label>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +48,12 @@
 export default {
   name: 'PaymentDetails',
   props: [],
+  data() {
+    return {
+      cbtAmount: null,
+      usdAmount: null
+    }
+  },
   components: {
   },
   computed: {
@@ -26,5 +71,25 @@ export default {
   line-height: 22px;
   text-align: center;
   padding: 20px 10px;
+
+  .cbt-title {
+    color: #258C42;
+  }
+
+  .title-usd-amount {
+    margin-top: 25px;
+    padding-left: 20px;
+    margin-left: 30px;
+  }
+
+  .title-usd-input {
+    padding-left: 20px;
+    margin-left: 30px;
+  }
+
+  .input-usd-amount {
+    padding-left: 20px;
+    margin-left: 35px;
+  }
 }
 </style>
