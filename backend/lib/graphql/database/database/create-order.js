3cbd8077521c72f5be444242aef79e57ba55976a
Omar Shibli <omarshib@gmail.com>
2017-09-04 10:21:10 +0300

    added generatePaymentAddress

--- backend/lib/graphql/database/database/create-order.js
+++ backend/lib/graphql/database/database/create-order.js
@@ -6,20 +6,21 @@ import { event_type, order_status } from '../../../../model/consts';
 import { generatePaymentAddress } from '../../../../lib/wallet';
 
 export default async (userId, orderInput) => {
-  const eventId = createOrderedId();
-  const paymentAddress = generatePaymentAddress(userId, orderInput.coin);
-  const payload = {
-    user_id: userId,
-    event_id: eventId,
-    type: event_type.order_created,
-    timestamp: new Date().toISOString(),
-    data: {
-      usd_amount: orderInput.usdAmount,
-      coin: orderInput.coin,
-      payment_address: paymentAddress,
-    },
-  };
-  return saveEvent(payload)
+  return generatePaymentAddress(userId, orderInput.coin)
+    .then(paymentAddress => {
+      const payload = {
+        user_id: userId,
+        event_id: createOrderedId(),
+        type: event_type.order_created,
+        timestamp: new Date().toISOString(),
+        data: {
+          usd_amount: orderInput.usdAmount,
+          coin: orderInput.coin,
+          payment_address: paymentAddress,
+        },
+      };
+      return saveEvent(payload);
+    })
     .then(payload => ({
       usdAmount: payload.data.usd_amount,
       coin: payload.data.coin,
